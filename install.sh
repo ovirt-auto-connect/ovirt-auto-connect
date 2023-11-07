7435906ea11fcf532eaeb702ef9767d2669d9f46
Oleg Primachenko <oleg.primachenko@eltex.loc>
2023-11-07 09:56:40 +0700

    add python and pip check

--- install.sh
+++ install.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+if which python > /dev/null 2>&1; then
+	python_version=`python --version 2>&1 | awk '{print $2}'`
+	echo "Python version $python_version is installed"
+else
+	echo "No Python executable is found"
+	exit 1
+fi
+
+if which pip > /dev/null 2>&1; then
+	pip_version=`pip --version 2>&1 | awk '{print $2}'`
+	echo "pip version $python_version is installed"
+else
+	echo "No pip executable is found"
+	exit 1
+fi
+
 pip install -r requirements.txt
 sudo install -m 755 "ovirt-auto-connect.py" /usr/local/bin/ovirt-auto-connect
 sudo mkdir -p /usr/share/applications
